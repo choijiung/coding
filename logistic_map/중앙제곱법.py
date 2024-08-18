1b851f75d0c911c71bafb3542e06cb4cd63ece07
Jiung <100206872+choijiung@users.noreply.github.com>
2024-08-18 16:43:35 +0900

    중앙제곱법 프로그램 수정
    시드값 정할 수 있음

--- logistic_map/중앙제곱법.py
+++ logistic_map/중앙제곱법.py
@@ -1,14 +1,16 @@
 import time
 def random_num():
-    seed = time.time()*10000
+    seed =  6100 #time.time()*10000
     seed_str = str(seed)
-    seed_digits = int(seed_str[10:14])
-    for i in range(1,5):
-        seed_square = seed_digits**2
+    middle_num = int(seed_str[0:4])#10:14])
+    for i in range(1, 10):
+        seed_square = middle_num**2
+        print(seed_square)
+        print(middle_num)
         seed_square_str = str(seed_square)
         length = len(seed_square_str)
         start_index = int((length-4)/2)
-        middle_num = seed_square_str[start_index:start_index+4]
-    print(middle_num)
-for j in range(1, 20):
-    random_num()
+        print(start_index)
+        middle_num = int(seed_square_str[start_index:start_index+4])
+    return middle_num
+print(random_num())
